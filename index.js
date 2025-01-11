32829a22af4bfde6166dc1c801df064ec6e58b73
Abdullah Al Ashik <abdullahashik17@gmail.com>
2025-01-12 03:35:22 +0600

    create server and use option to find specific data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middlewire
@@ -27,6 +27,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // get all services data 
+    app.get('/services',async(req,res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // get specific sewrvice data
+    app.get('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const quary = {_id : new ObjectId(id)}
+
+      const options = {
+        
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1 , service_id : 1},
+      };
+
+      const result = await serviceCollection.findOne(quary,options);
+      res.send(result)
+    })
+
+    const serviceCollection = client.db('carDoctor').collection('services');
 
 
     // Send a ping to confirm a successful connection
